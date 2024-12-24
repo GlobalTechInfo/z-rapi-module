c278e2ef340009ff8c6408a0218cbd18c14060d9
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-24 17:55:17 +0500

    Update tes.js

--- tes.js
+++ tes.js
@@ -1,52 +1,13 @@
-const zrapi = require("./src/index.js");
+const axios = require('axios');
+const cheerio = require('cheerio');  // Install with 'npm install cheerio'
 
-//TextPro
-zrapi
-  .textpro("https://textpro.me/create-blackpink-logo-style-online-1001.html", [
-    "teks",
-  ])
-  .then((data) => {
-    console.log("TextPro Response:", data);
+axios.get("https://textpro.me/create-blackpink-logo-style-online-1001.html")
+  .then(response => {
+    const $ = cheerio.load(response.data);
+    // Scrape the required data from the page
+    const result = $("selector-for-url").attr("href");  // Modify selector accordingly
+    console.log(result);
   })
-  .catch((err) => {
-    console.error("TextPro Error:", err);
-  });
-
-//TextPro with 2 text
-zrapi
-  .textpro(
-    "https://textpro.me/create-glitch-text-effect-style-tik-tok-983.html",
-    ["teks", "teks 2"]
-  )
-  .then((data) => {
-    console.log("TextPro with 2 texts Response:", data);
-  })
-  .catch((err) => {
-    console.error("TextPro with 2 texts Error:", err);
-  });
-
-//Photooxy
-zrapi
-  .photooxy(
-    "https://photooxy.com/logo-and-text-effects/create-a-picture-of-love-message-377.html",
-    ["teks"]
-  )
-  .then((data) => {
-    console.log("Photooxy Response:", data);
-  })
-  .catch((err) => {
-    console.error("Photooxy Error:", err);
-  });
-
-//Photooxy with 2 text
-zrapi
-  .photooxy(
-    "https://photooxy.com/logo-and-text-effects/make-tik-tok-text-effect-375.html",
-    ["teks", "Teks 2"]
-  )
-  .then((data) => {
-    console.log("Photooxy with 2 texts Response:", data);
-  })
-  .catch((err) => {
-    console.error("Photooxy with 2 texts Error:", err);
+  .catch(error => {
+    console.log('Error:', error);
   });
