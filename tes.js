4c95e7a7222e6080f3450a711e47a2a2ad80777c
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-24 16:35:15 +0500

    Update tes.js

--- tes.js
+++ tes.js
@@ -1,6 +1,16 @@
 const zrapi = require("./src/index");
 
-// zrapi.pastegg("Z", )
+// Fetching data from Snaptik (likely to download or extract video)
 zrapi.snaptik("https://vt.tiktok.com/khpq9t")
-.then(data => console.log(data))
-.catch(e => console.log(e))
+  .then(data => console.log(data))
+  .catch(e => console.error(e));
+
+// Fetching data from Textpro (likely to apply some text effects on the video or URL)
+zrapi.textpro("https://vt.tiktok.com/khpq9t")
+  .then(data => console.log(data))
+  .catch(e => console.error(e));
+
+// Fetching data from Photooxy (likely to apply photo effects)
+zrapi.photooxy("https://vt.tiktok.com/khpq9t")
+  .then(data => console.log(data))
+  .catch(e => console.error(e));
