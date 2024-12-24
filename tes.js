c9969e5030260abeaab50c8eb77231fae150f899
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-24 16:40:23 +0500

    Update tes.js

--- tes.js
+++ tes.js
@@ -6,11 +6,13 @@ zrapi.snaptik("https://vt.tiktok.com/khpq9t")
   .catch(e => console.error(e));
 
 // Fetching data from Textpro (likely to apply some text effects on the video or URL)
-zrapi.textpro("https://vt.tiktok.com/khpq9t")
+zrapi.textpro("https://textpro.me/create-blackpink-logo-style-online-1001.html")
+  ["text"]
   .then(data => console.log(data))
   .catch(e => console.error(e));
 
 // Fetching data from Photooxy (likely to apply photo effects)
-zrapi.photooxy("https://vt.tiktok.com/khpq9t")
+zrapi.photooxy("https://textpro.me/create-glitch-text-effect-style-tik-tok-983.html")
+  ["text, text2"]
   .then(data => console.log(data))
   .catch(e => console.error(e));
