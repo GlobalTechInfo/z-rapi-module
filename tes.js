a32df7506d5c40789fcc9b3e4ba9a8d1ddf31133
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-24 17:53:25 +0500

    Update tes.js

--- tes.js
+++ tes.js
@@ -5,8 +5,12 @@ zrapi
   .textpro("https://textpro.me/create-blackpink-logo-style-online-1001.html", [
     "teks",
   ])
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+  .then((data) => {
+    console.log("TextPro Response:", data);
+  })
+  .catch((err) => {
+    console.error("TextPro Error:", err);
+  });
 
 //TextPro with 2 text
 zrapi
@@ -14,8 +18,12 @@ zrapi
     "https://textpro.me/create-glitch-text-effect-style-tik-tok-983.html",
     ["teks", "teks 2"]
   )
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+  .then((data) => {
+    console.log("TextPro with 2 texts Response:", data);
+  })
+  .catch((err) => {
+    console.error("TextPro with 2 texts Error:", err);
+  });
 
 //Photooxy
 zrapi
@@ -23,8 +31,12 @@ zrapi
     "https://photooxy.com/logo-and-text-effects/create-a-picture-of-love-message-377.html",
     ["teks"]
   )
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+  .then((data) => {
+    console.log("Photooxy Response:", data);
+  })
+  .catch((err) => {
+    console.error("Photooxy Error:", err);
+  });
 
 //Photooxy with 2 text
 zrapi
@@ -32,5 +44,9 @@ zrapi
     "https://photooxy.com/logo-and-text-effects/make-tik-tok-text-effect-375.html",
     ["teks", "Teks 2"]
   )
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+  .then((data) => {
+    console.log("Photooxy with 2 texts Response:", data);
+  })
+  .catch((err) => {
+    console.error("Photooxy with 2 texts Error:", err);
+  });
